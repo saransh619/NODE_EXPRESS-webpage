835b223da563b48f08c008581f0450cc4ca7044e
saransh pachhai <63485889+saransh619@users.noreply.github.com>
2022-02-24 11:05:31 +0545

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -19,7 +19,7 @@ const getInfo = async(event) => {
         try{
            
 
-            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=b14425a6554d189a2d7dc18a8e7d7263`
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=useyourAPIkeyhere`
             const response = await fetch(url);
 
             const data = await response.json();
